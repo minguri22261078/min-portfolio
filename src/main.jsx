047c70b89f95309ae5a231a54cbf953c66dc3d43
minguri <elmo0915@kaywon.ac.kr>
2025-09-08 15:31:34 +0900

    404 error 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'; // 라우터를 사용하기 위해 추가
+import { HashRouter } from 'react-router-dom'; // GitHub Pages에서 새로고침 404 방지
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter basename="/rect-portfolio/">
+        <HashRouter>
             {/* 라우터로 앱을 감싸기 */}
             {/* basename에 Repository 경로를 추가 */}
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>,
 )
 

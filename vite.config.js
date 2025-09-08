89e28e5bee32285a9d2d4f535b9b5a89f7e49b12
minguri <elmo0915@kaywon.ac.kr>
2025-09-08 15:12:39 +0900

    Update src for feature/page routing

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: "/min-portfolio/", // 여기에 Repository 경로를 추가
 })

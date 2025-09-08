32b9bb208982603aad2beb6260f830bc874339e0
minguri <elmo0915@kaywon.ac.kr>
2025-09-08 15:41:09 +0900

    react-portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'; // GitHub Pages에서 새로고침 404 방지
+import { BrowserRouter } from 'react-router-dom'; // 라우터를 사용하기 위해 추가
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <HashRouter>
+        <BrowserRouter basename="/min-portfolio/">
             {/* 라우터로 앱을 감싸기 */}
             {/* basename에 Repository 경로를 추가 */}
             <App />
-        </HashRouter>
+        </BrowserRouter>
     </StrictMode>,
 )
-
